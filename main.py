fc21ec74dcc95a24b957103dd250cff2cbbf9ec5
Timothy Hernandez <timhernand08@gmail.com>
2024-12-18 12:36:23 -0500

    Removed confirmation for exit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from memory import storeData, hasJoke, hasQuote, update_time_trigger
+from memory import storeData, update_time_trigger
 from initdb import initialize_db
 from gui import App, get_value
 from tkinter.messagebox import askyesno
@@ -9,12 +9,12 @@ initialize_db()
 
 get_gen = App()
 
-def confirm():
+'''def confirm():
     ans = askyesno(title = 'Exit', message='Do You Want To Exit?')
     if ans:
         get_gen.destroy()
 
-get_gen.protocol("WM_DELETE_WINDOW", confirm)
+get_gen.protocol("WM_DELETE_WINDOW", confirm)'''
 get_gen.mainloop()
 
 

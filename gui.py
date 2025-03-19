3ac8e4ac16945d3f43b9be9c80173bb034929dca
Timothy Hernandez <timhernand08@gmail.com>
2025-03-19 12:23:06 -0400

    Added Threading. Program will now start with "loading" and wait until API returns values

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import customtkinter
 from generator import checker
 import os
 import sys
+import threading
 
 def resource_path(relative_path):
     try:
@@ -17,23 +18,32 @@ class ButtonFrame(customtkinter.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.value = value
         self.title= title
-        text = checker(self.value)
 
         button = customtkinter.CTkButton(self, text=title, command=self.get_button)
         button.grid(row = 0, column = 0, padx=10, pady=(10, 0), sticky = "ew")
 
         self.text = customtkinter.CTkTextbox(self, height=100, wrap="word")
         self.text.grid(row = 1, column = 0, padx=10, pady=(10, 0), sticky = "nsew")
-        self.text.insert("0.0", f"{text}")
+        self.text.insert("0.0", "Loading...")
         self.text.configure(state="disabled")
-        set_value(self.value, text)
+        
+        threading.Thread(target=self.load_initial_text).start()
 
-    def get_button(self):
+    def load_initial_text(self):
         text = checker(self.value)
+        self.update_text(text)
+        set_value(self.value, text)
+    
+    def update_text(self, text):
         self.text.configure(state="normal")
         self.text.delete("0.0", "end")
         self.text.insert("0.0", f"{text}")
         self.text.configure(state="disabled")
+
+
+    def get_button(self):
+        text = checker(self.value)
+        self.update_text(text)
         set_value(self.value, text)
 
     def get(self):
@@ -53,10 +63,10 @@ class App(customtkinter.CTk):
 
         self.quote_button = ButtonFrame(self, title="Generate Quote", value="quote")
         self.quote_button.grid(row = 0, column = 0, padx=10, pady=10, sticky = "nsew")
-
+        
         self.joke_button = ButtonFrame(self, title = "Generate Joke", value="joke")
         self.joke_button.grid(row=0, column = 1, padx=10, pady=10, sticky="nsew")
-
+        
 
     def get_quote(self):
         return self.quote_button.get()
